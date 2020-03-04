e4f99ef71ae1e5f779ff89c5d103891576412991
Aman kumar <32014475+Dev-Aman7@users.noreply.github.com>
2020-03-04 21:52:22 +0530

    Add files via upload

---
+++ mouse_joy.ino
@@ -0,0 +1,30 @@
+int x,y,z;
+
+void joy()
+{
+  x=analogRead(A0);
+  y=analogRead(A1);
+  if(  x<600 && y>900)
+  {Serial.println("up");}
+else if(x<600 && y<200)
+  {Serial.println("down");}
+else if(x<400 && y<600)
+  {Serial.println("left");}
+else if(x>600 && y<600)
+  {Serial.println("right");}
+else
+  {Serial.println("stop");}
+  Serial.print(x);
+  Serial.print("  ");
+  Serial.println(y);
+  //Serial.println("Down");
+}
+void setup() {
+  Serial.begin(9600);
+
+}
+//bool button=false;
+void loop() {
+  joy();
+  delay(100);
+}
